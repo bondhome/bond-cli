8f0d06254fc73768a57e121b3c1ff59871cdb5d3
Jacob Lambert <jacob.lambert@gmail.com>
2020-02-24 13:11:15 -0500

    Request the token when selecting a Bond (#10)

--- bond/commands/select.py
+++ bond/commands/select.py
@@ -1,11 +1,20 @@
 from .base_command import BaseCommand
+from .token import update_token
 import bond.database
 from ..cli.console import LogLine
+from bond.proto import get_async
+
+
+def update_token_callback(bondid, rsp):
+    token = rsp.get("b", {}).get("token")
+    if token:
+        print(f"{bondid}'s token is unlocked, updating...")
+        update_token(token)
 
 
 class SelectCommand(BaseCommand):
     subcmd = "select"
-    help = "Select a single Bond to interact with."
+    help = "Select a single Bond to interact with. If the token of this Bond is unlocked, it will be set. (The easiest way to unlock a token is with a power cycle)"
     arguments = [
         (
             ["BONDID"],
@@ -25,6 +34,10 @@ class SelectCommand(BaseCommand):
             if args.port:
                 bond.database.set_bond(args.BONDID, "port", args.port)
                 LogLine("Set %s port %s" % (args.BONDID, args.ip))
+            threads = get_async(
+                args.BONDID, topic="token", on_success=update_token_callback
+            )
+
         if args.none:
             bond.database.set("selected_bondid", None)
         LogLine("Selected Bond: %s" % bond.database.get("selected_bondid"))
