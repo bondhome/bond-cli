959d430aa5b3df20a1eff811c53a8ff03ccea26f
Chris Merck <chrismerck@gmail.com>
2020-03-18 13:48:03 -0400

    tweak type=rescue help text

--- bond/commands/reset.py
+++ bond/commands/reset.py
@@ -12,10 +12,11 @@ class ResetCommand(BaseCommand):
             "help": """setup: clear the WiFi database record, making the Bond
                        disconnect from WiFi and allowing it to be set up again\n
                        factory: clear the Bond's entire database\n
-                       rescue: clear the Bond's entire database and reset its firmware
-                       to factory settings (this is NOT RECOMMENDED unless you really
+                       rescue: clear the Bond's entire database and restore the original
+                       firmware version that the unit shipped with. 
+                       This is NOT RECOMMENDED unless you really
                        know what you're doing! You probably don't need this unless
-                       you're developing firmware for the Bond)""",
+                       you're developing firmware for the Bond.""",
             "choices": ["setup", "factory", "rescue"],
         }
     }
