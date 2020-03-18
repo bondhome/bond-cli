d6b7c2c01190b56de92628553775e516d0006c2d
Jacob Lambert <jacob.lambert@gmail.com>
2020-03-18 14:00:40 -0400

    Setup Pypi package; Test and deploy it in CI (#21)

---
+++ bond/database/test_database.py
@@ -0,0 +1,6 @@
+from bond.database import BondDatabase
+
+def test_database_get_and_set():
+  for i in range(5):
+    BondDatabase.set("test", i)
+    assert BondDatabase.get("test") == i
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+DEPS_ALL = open('requirements.txt').readlines()
+
+DEPS_TEST = DEPS_ALL + open('requirements-test.txt').readlines()
+
+setup(
+    name='bond-cli',
+    version='0.0.3',
+    author='Olibra',
+    packages=find_packages(),
+    scripts=['bond/bond'],
+    include_package_data=True,
+    description='Bond CLI',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    python_requires='>=3.6',
+    setup_requires=['pytest-runner'],
+    install_requires=DEPS_ALL,
+    tests_require=DEPS_TEST
+)
