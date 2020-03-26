9abb3e0bb38f2c2987de0b78c89e8017dec9e73e
Jacob Lambert <jacob.lambert@gmail.com>
2020-03-25 20:16:52 -0400

    Tweak the timing of the devices error message

--- bond/commands/devices.py
+++ bond/commands/devices.py
@@ -12,9 +12,9 @@ class DevicesCommand(BaseCommand):
 
     def run(self, args):
         bond_id = BondDatabase.get_assert_selected_bondid()
+        dev_ids = bond.proto.get(bond_id, topic="devices").get("b", {})
         print("Devices on %s" % bond_id)
         with Table(["dev_id", "name", "location"]) as table:
-            dev_ids = bond.proto.get(bond_id, topic="devices").get("b", {})
             dev_ids.pop("_", None)
             for dev_id in dev_ids:
                 dev = bond.proto.get(bond_id, topic="devices/%s" % dev_id).get("b", {})
