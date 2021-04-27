0e38eb8e66e302ea74ff311abf12b6aad9426bef
Chris Merck <chrismerck@gmail.com>
2021-04-27 12:34:29 -0400

    reboot: add --cold option

--- bond/commands/reboot.py
+++ bond/commands/reboot.py
@@ -7,12 +7,19 @@ class RebootCommand(BaseCommand):
     subcmd = "reboot"
     help = """Reboot a Bond. No reset is performed."""
     arguments = {
+        "--cold": {
+            "help": "simulate cold boot",
+            "action": "store_true"
+        }
     }
 
     def run(self, args):
         bondid = BondDatabase.get_assert_selected_bondid()
         try:
-            bond.proto.put(bondid, topic="sys/reboot", body={})
+            body = {}
+            if args.cold:
+                body['type'] = 'cold'
+            bond.proto.put(bondid, topic="sys/reboot", body=body)
         except requests.exceptions.ReadTimeout:
             print("Connection timed out, as expected.")
 
