281bc7919ce3cb7d1b63e6a649fc87b5ba4999a3
Frederico Jordan <fredericojordan@gmail.com>
2022-01-27 11:02:45 -0500

    style: Revert Black changes that broke flake8

--- bond/proto/__init__.py
+++ bond/proto/__init__.py
@@ -1,9 +1,1 @@
-from bond.proto.wye import (
-    get,
-    put,
-    post,
-    patch,
-    delete,
-    get_all_async,
-    get_async,
-)  # noqa: F401
+from bond.proto.wye import get, put, post, patch, delete, get_all_async, get_async  # noqa: F401
