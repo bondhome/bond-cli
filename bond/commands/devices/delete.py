3132454210dbb5bd03748fa38c5b4839a3eb8668
Frederico Jordan <fredericojordan@gmail.com>
2022-01-31 14:00:36 -0500

    feat: Allow for multiple deletions at same time

--- bond/commands/devices/delete.py
+++ bond/commands/devices/delete.py
@@ -15,7 +15,7 @@ class DeviceDeleteCommand(object):
 
     def setup(self, parser):
         group = parser.add_mutually_exclusive_group()
-        group.add_argument("--device-id", help="ID of the device being deleted")
+        group.add_argument("--device-ids", help="ID of the device(s) being deleted", nargs="*")
         group.add_argument("--all", help="delete all devices", action="store_true")
 
     def run(self, args):
@@ -26,13 +26,14 @@ class DeviceDeleteCommand(object):
                 or input(f"Delete all devices from {bond_id}? [N/y] ").lower() == "y"
             ):
                 self.delete_all_devices(bond_id)
-        elif args.device_id:
+        elif args.device_ids:
             if (
                 args.force
-                or input(f"Delete device {args.device_id}? [N/y] ").lower() == "y"
+                or input(f"Delete device(s) {args.device_ids}? [N/y] ").lower() == "y"
             ):
-                bond.proto.delete(bond_id, topic=f"devices/{args.device_id}")
-                print(f"{args.device_id} device deleted.")
+                for dev_id in args.device_ids:
+                    bond.proto.delete(bond_id, topic=f"devices/{dev_id}")
+                    print(f"{dev_id} device deleted.")
 
     def delete_all_devices(self, bond_id):
         dev_ids = bond.proto.get(bond_id, topic="devices").get("b", {})
--- bond/commands/groups/delete.py
+++ bond/commands/groups/delete.py
@@ -15,7 +15,7 @@ class GroupDeleteCommand(object):
 
     def setup(self, parser):
         group = parser.add_mutually_exclusive_group()
-        group.add_argument("--group_id", help="ID of the group being deleted")
+        group.add_argument("--group-ids", help="ID of the group(s) being deleted", nargs="*")
         group.add_argument("--all", help="delete all groups", action="store_true")
 
     def run(self, args):
@@ -26,13 +26,14 @@ class GroupDeleteCommand(object):
                 or input(f"Delete all groups from {bond_id}? [N/y] ").lower() == "y"
             ):
                 self.delete_all_groups(bond_id)
-        elif args.group_id:
+        elif args.group_ids:
             if (
                 args.force
-                or input(f"Delete group {args.group_id}? [N/y] ").lower() == "y"
+                or input(f"Delete group(s) {args.group_ids}? [N/y] ").lower() == "y"
             ):
-                bond.proto.delete(bond_id, topic=f"groups/{args.group_id}")
-                print(f"{args.group_id} group deleted.")
+                for group_id in args.group_ids:
+                    bond.proto.delete(bond_id, topic=f"groups/{group_id}")
+                    print(f"{group_id} group deleted.")
 
     def delete_all_groups(self, bond_id):
         groups_ids = bond.proto.get(bond_id, topic="groups").get("b", {})
