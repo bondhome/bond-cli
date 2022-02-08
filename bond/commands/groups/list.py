a636d543ff462a6df1bd2cb04ad1434339a19b85
Frederico Jordan <fredericojordan@gmail.com>
2022-02-08 15:06:19 -0300

    feat: Parallelize group listing

--- bond/commands/groups/list.py
+++ bond/commands/groups/list.py
@@ -1,6 +1,5 @@
 import bond.proto
 from bond.cli.table import Table
-from bond.database import BondDatabase
 
 
 class GroupsListCommand(object):
@@ -15,16 +14,25 @@ class GroupsListCommand(object):
     }
 
     def run(self, args):
-        groups = {}
-        for bond_id in BondDatabase.get_bonds():
-            try:
-                group_shards = bond.proto.get(bond_id, topic="groups").get("b", {})
-                for shard_id in group_shards.keys():
-                    if shard_id.startswith("_"):
-                        continue
-                    groups[shard_id] = groups.get(shard_id, []) + [bond_id]
-            except Exception:
-                pass
         table = Table(["group_id", "bonds"], quiet=args.q)
-        for group, bonds in groups.items():
-            table.add_row({"group_id": group, "bonds": ",".join(bonds)})
+        groups = {}
+
+        for thread in bond.proto.get_all_async(
+            topic="groups",
+            on_success=lambda bond_id, response: include_group_shards(
+                groups, bond_id, response.get("b", {})
+            ),
+            on_error=lambda _bond_id, _error_msg: None,
+        ):
+            thread.join()
+
+        for group_id, bonds in groups.items():
+            table.add_row({"group_id": group_id, "bonds": ",".join(bonds)})
+
+
+def include_group_shards(groups, bond_id, group_shards):
+    group_ids = [
+        group_id for group_id in group_shards.keys() if not group_id.startswith("_")
+    ]
+    for group in group_ids:
+        groups[group] = groups.get(group, []) + [bond_id]
