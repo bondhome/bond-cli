ef9cc3e8c39fb0350f4eb49a88260736b719974f
Frederico Jordan <fredericojordan@gmail.com>
2022-04-12 13:38:03 -0300

    build: Bump version to 0.2.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ DEPS_TEST = DEPS_ALL + open("requirements-test.txt").readlines()
 
 setup(
     name="bond-cli",
-    version="0.1.1",
+    version="0.2.1",
     author="Olibra",
     packages=find_packages(),
     scripts=["bond/bond"],
